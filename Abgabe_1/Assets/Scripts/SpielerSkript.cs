41d9c00c94e4744fb58e3712da7c69101b3c6123
soosaasees <nacho@DESKTOP-8S1QLVE.localdomain>
2021-05-06 20:27:31 +0200

    Quaternions und diesmal auch die Formel hoffentlich richtig implementiert

--- Abgabe_1/Assets/Scripts/SpielerSkript.cs
+++ Abgabe_1/Assets/Scripts/SpielerSkript.cs
@@ -6,6 +6,7 @@ public class SpielerSkript : MonoBehaviour
 {
     private float rotSpeed=1;
     private float moveSpeed=5;
+    private float rotDirection=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,16 +21,13 @@ public class SpielerSkript : MonoBehaviour
         bool rotateRight=Input.GetKey("e");
         Vector3 moveDirection=transform.rotation* new Vector3(moveHorizontal, 0, moveVertical)  ;
         transform.position+=moveDirection*moveSpeed*Time.deltaTime;
-        float rotDirection;
         if(rotateLeft&!rotateRight) {
-            rotDirection=-100;
+           rotDirection-=rotSpeed*Time.deltaTime;
         }
         else if(rotateRight&!rotateLeft)    {
-            rotDirection=100;
+          rotDirection+=rotSpeed*Time.deltaTime;
         }
-        else rotDirection=0;
-        Vector3 rotationVector=new Vector3(0, rotDirection, 0);
-        Quaternion targetDirection=Quaternion.Euler(rotationVector)*transform.rotation;
-       transform.rotation=Quaternion.Lerp(transform.rotation, targetDirection, rotSpeed*Time.deltaTime);
+        Vector3 targetDirection=new Vector3(Mathf.Sin(rotDirection), 0, Mathf.Cos(rotDirection));
+       transform.rotation=Quaternion.LookRotation(targetDirection);
     }
 }
